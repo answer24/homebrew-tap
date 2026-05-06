7b71711f6732219a87a144ebdad9fa77749679a2
answer24 <barry.wu.china@gmail.com>
2026-05-06 19:50:22 +0800

    fix brooklyn 2.1.3 zip structure and update sha256

--- Casks/brooklyn.rb
+++ Casks/brooklyn.rb
@@ -1,6 +1,6 @@
 cask "brooklyn" do
   version "2.1.3"
-  sha256 "6ed5793a50c18c6e90093e2a738eb8abcb7511ede03a4f958aa3eba5567ca0e5"
+  sha256 "5df679dbc0540c45874aef949572ee3da2d636798894249b0c03343db64ba490"
 
   url "https://github.com/answer24/Brooklyn/releases/download/v#{version}/Brooklyn.saver.zip"
   name "Brooklyn"
